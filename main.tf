04f2da7a55a0f90d5a84710e2a44334a958b4cfe
Kinyaev Ilfat <kinyaevilfat@gmail.com>
2018-10-29 10:02:36 +0300

    Capacity changed

--- main.tf
+++ main.tf
@@ -74,7 +74,7 @@ resource "aws_autoscaling_group" "asgPrimary" {
   depends_on = ["aws_launch_configuration.primary"]
   availability_zones        = ["${data.aws_availability_zones.available.names[0]}"]
   name                      = "TerraformASGPrimary"
-  max_size                  = 1
+  max_size                  = 2
   min_size                  = 1
   wait_for_capacity_timeout = "0"
   health_check_grace_period = 300
@@ -88,7 +88,6 @@ resource "aws_autoscaling_group" "asgPrimary" {
 }
 
 resource "aws_subnet" "private_primary" {
-  # depends_on = ["aws_subnet.secondary"]
   depends_on = ["aws_subnet.primary"]
   availability_zone = "${data.aws_availability_zones.available.names[0]}"
   vpc_id = "${aws_vpc.vpc.id}"
@@ -147,8 +146,7 @@ resource "aws_eip" "nat" {
 }
 
 resource "aws_elb" "elb" {
-  # depends_on = ["aws_subnet.primary", "aws_subnet.secondary", "aws_nat_gateway.gw"]
-  depends_on = ["aws_subnet.primary", "aws_nat_gateway.gw"]
+  depends_on = ["aws_subnet.primary", "aws_subnet.secondary", "aws_nat_gateway.gw"]
   name               = "ELB"
   security_groups = ["${aws_security_group.sg-elb.id}"]
 
@@ -167,8 +165,7 @@ resource "aws_elb" "elb" {
     interval            = 60
   }
 
-  subnets = ["${aws_subnet.primary.id}"]
-  # subnets = ["${aws_subnet.primary.id}", "${aws_subnet.secondary.id}"]
+  subnets = ["${aws_subnet.primary.id}", "${aws_subnet.secondary.id}"]
   cross_zone_load_balancing   = true
   idle_timeout                = 400
   connection_draining         = true
@@ -243,3 +240,92 @@ resource "aws_s3_bucket_policy" "static_files_bucket_policy" {
 #   service_name = "com.amazonaws.us-west-2.s3"
 #   route_table_ids = ["${aws_route_table.rt.id}"]
 # }
+
+resource "aws_autoscaling_group" "asgSecondary" {
+  depends_on = ["aws_launch_configuration.secondary"]
+  availability_zones        = ["${data.aws_availability_zones.available.names[1]}"]
+  name                      = "TerraformASGSecondary"
+  max_size                  = 2
+  min_size                  = 1
+  wait_for_capacity_timeout = "0"
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+  wait_for_capacity_timeout = "0"
+  force_delete              = false
+  vpc_zone_identifier = ["${aws_subnet.secondary.id}"]
+  launch_configuration      = "${aws_launch_configuration.secondary.name}"
+  load_balancers = ["${aws_elb.elb.name}"]
+}
+
+resource "aws_subnet" "private_secondary" {
+  depends_on = ["aws_subnet.secondary"]
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "${var.secondary_private_subnet_cidr}"
+  map_public_ip_on_launch = "${var.private_ip_bool}"
+  tags {
+    Name = "Private Secondary"
+  }
+}
+
+resource "aws_subnet" "secondary" {
+  depends_on = ["aws_route_table.rt"]
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "${var.secondary_public_subnet_cidr}"
+  map_public_ip_on_launch = "${var.public_ip_bool}"
+  tags {
+    Name = "Secondary"
+  }
+}
+
+resource "aws_route_table_association" "b" {
+  depends_on = ["aws_route_table.rt", "aws_subnet.secondary"]
+  subnet_id      = "${aws_subnet.secondary.id}"
+  route_table_id = "${aws_route_table.rt.id}"
+}
+
+resource "aws_security_group" "secondary" {
+  name        = "${var.secondary_sg}"
+  description = "${var.secondary_sg_desc}"
+  depends_on = ["aws_internet_gateway.gw"]
+  vpc_id = "${aws_vpc.vpc.id}"
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  tags {
+    Name = "${var.secondary_sg}"
+  }
+}
+
+resource "aws_launch_configuration" "secondary" {
+  image_id      = "ami-030aae8cba933aede"
+  instance_type = "${var.ami_type}"
+  security_groups = ["${aws_security_group.primary.id}"]
+  user_data = 
+  <<-EOF
+  #!/bin/bash
+  # curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P chefdk -c stable -v 2.5.3
+  # mkdir chef-repo
+  # cd chef-repo
+  curl https://raw.githubusercontent.com/KILFaT/aws-scalable-app/master/serv.py -O
+  # sudo chef-client --local-mode webserver.rb
+  python serv.py
+  # python -m http.server 80
+  EOF
+
+  key_name               = "${var.key_name}"
+  depends_on = ["aws_subnet.secondary"]
+  name          = "web_config_secondary"
+}
